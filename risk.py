2927f34327c571e4e65e167ed8434be4822480b6
clhliu <lhliu@sfu.ca>
2013-01-27 21:43:45 -0800

    commands.promt_user() to risk.commands.prompt_user()

--- risk.py
+++ risk.py
@@ -32,7 +32,7 @@ def game_setup(settings):
 
 def run_game(board, settings):
     print 'Game ran!'
-    return commands.prompt_user()
+    return risk.commands.prompt_user()
 
 if __name__ == '__main__':
     settings = app_setup()
