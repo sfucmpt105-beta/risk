d0a1d2b8fe50fb2cfec22fd496df065e0dfad812
Curtis <lhliu@sfu.ca>
2013-02-09 19:12:55 -0800

    -added attack command -splits the command -need to figure out how to make it so the "attack" function runs even though it has additional information after the command -added attack_help, fortify_help. Explains the syntax to the user -added add_unit, the user types "add [unit]" and sorts the which unit. (need to implement the targeted territory -included "user_input" into args for functions

--- risk/commands.py
+++ risk/commands.py
@@ -3,11 +3,11 @@ import risk.logger
 #Constants
 _INVALID_INITIAL_INPUT = None
 
-def help_info(player, game_master):
+def help_info(player, game_master, user_input):
     print 'Available commands:'
     print '%s'%user_commands.keys()
     
-def status_info(player, game_master):
+def status_info(player, game_master, user_input):
     print 'status'
     print 'Player %s:\n' % player 
     print 'Territories:'
@@ -16,43 +16,76 @@ def status_info(player, game_master):
     infantries  = 0
     artilleries = 0
     for territory in territories:
-	cavalries   = territory.cavalries
-	infantries  = territory.infantries
-	artilleries = territory.artilleries
-	print '[%s]:\n' \
-	'Cavlries: %s\n' \
-	'Infantries: %s\n' \
-	'Artilleries: %s\n' % (territory, calvaries, infantries, artilleries)
+        cavalries   = territory.cavalries
+        infantries  = territory.infantries
+        artilleries = territory.artilleries
+        print '[%s]:\n' \
+        'Cavlries: %s\n' \
+        'Infantries: %s\n' \
+        'Artilleries: %s\n                              ' % (territory, calvaries, infantries, artilleries)
 
-def next_info(player, game_master):
+def next_info(player, game_master, user_input):
     risk.logger.debug('User finished turn')
     print 'next'
 
-def territories_info(player, game_master):
+def territories_info(player, game_master, user_input):
     print 'territories'
     # list of territories
-	
-def attack_info(player, game_master):
+    
+def attack_info(player, game_master, user_input):
     print 'attack!'
-	
-def fortify_info(player, game_master):
-    print 'fortify!'
+    game_master.player_attack
 
-def print_info(player, game_master):
+def attack_help(player, game_master, user_input):
+    print 'To attack a player type:\n' \
+        '\"attack [territory_origin] [territory_target]\"\n'
+    
+def fortify_info(player, game_master, user_input):
+    print 'fortify!'
+    
+
+def fortify_help(player, game_master, user_input):
+    print 'To use fortify type:\n' \
+    '\"fortify [territory_origin] [territory_destination] [type] [#]\"\n' \
+    '[type] of unit please enter:\n' \
+    'i = infantry\n' \
+    'c = cavalry\n' \
+    'a = artilery\n'
+    
+def print_info(player, game_master, user_input):
     print 'print'
 
-def quit_game(player, game_master):
+def add_unit(player, game_master, user_input):
+    user_input = user_input
+    if user_input == 'add infantry':
+        #game_master.player_add_infantry
+        print 'Adding infantry'
+
+    if user_input == 'add cavalry':
+        #game_master.player_add_cavalry
+        print 'Adding cavalry'
+    
+    if user_input == 'add artilery':
+        #game_master.player_add_artilery
+        print 'Adding artilery'
+    
+def quit_game(player, game_master, user_input):
     risk.logger.debug('User wants to quit game')
     game_master.end_game()
 
 user_commands = {
-    'help':         help_info,           
+    'help':         help_info,
+    'help fortify': fortify_help,
+    'help attack':  attack_help,
     'status':       status_info,
     'next':         next_info,
     'territories':  territories_info,
     'attack':       attack_info,
     'fortify':      fortify_info,
     'print':        print_info,
+    'add infantry': add_unit,
+    'add cavalry':  add_unit,
+    'add artilery,':add_unit,
     'quit':         quit_game,
     }
 
@@ -61,7 +94,7 @@ def prompt_user(player, game_master):
     while not user_input_finished(user_input):
         try:    # verifies that it is a valid command in the list
             user_input = raw_input('[Please type a command]: ')
-            user_commands[user_input](player, game_master)  
+            user_commands[user_input](player, game_master, user_input)  
         except KeyError:
             print 'invalid command'
 
